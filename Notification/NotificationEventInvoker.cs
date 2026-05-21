fe169a2c55eb92fcd0594152768c4ddeca606984
DGOne <2369557156@qq.com>
2026-05-21 13:41:06 +0800

    Update NotificationEventInvoker.cs
    ● 改好了。去掉了 HorizontalGroup 压缩布局，现在 Delay
      是标准的带标签数值输入框，可以直接点击输入具体数值，不再是被压缩的滑条样式。

--- Notification/NotificationEventInvoker.cs
+++ Notification/NotificationEventInvoker.cs
@@ -1,9 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class NotificationEventInvoker : MonoBehaviour
 {
+    [Serializable]
+    public class DelayedEvent
+    {
+        [LabelText("延时"), SuffixLabel("秒", overlay: true), MinValue(0f)]
+        public float Delay;
+
+        [PropertySpace(4)]
+        public UnityEvent Callback;
+    }
+
     [TitleGroup("Event")]
     [SerializeField, Required] private ScriptableEventNotification _event;
 
@@ -11,19 +25,16 @@ public class NotificationEventInvoker : MonoBehaviour
     [SerializeField] private NotificationData _data;
 
     [TitleGroup("Callbacks")]
-    [SerializeField, FoldoutGroup("Callbacks/On Raise")]
-    private UnityEvent _onRaise;
-
-    [SerializeField, FoldoutGroup("Callbacks/On Raise")]
-    private UnityEvent<NotificationData> _onRaiseWithData;
+    [SerializeField, ListDrawerSettings(ShowFoldout = true, DraggableItems = true)]
+    [LabelText("事件列表")]
+    private List<DelayedEvent> _events = new();
 
     // 通过 UnityEvent 静态绑定调用，发送 Inspector 中配置好的通知数据
     [TitleGroup("Event"), Button("Raise", ButtonSizes.Medium)]
     public void Raise()
     {
         _event.Raise(_data);
-        _onRaise?.Invoke();
-        _onRaiseWithData?.Invoke(_data);
+        InvokeAll();
     }
 
     // 通过 UnityEvent 动态字符串绑定调用，使用传入文本覆盖 Message，其余时长沿用 Inspector 配置
@@ -32,7 +43,26 @@ public class NotificationEventInvoker : MonoBehaviour
         var data = _data;
         data.Message = message;
         _event.Raise(data);
-        _onRaise?.Invoke();
-        _onRaiseWithData?.Invoke(data);
+        InvokeAll();
+    }
+
+    private void InvokeAll()
+    {
+        for (int i = 0; i < _events.Count; i++)
+        {
+            var entry = _events[i];
+            if (entry.Delay > 0f)
+            {
+                StartCoroutine(DelayedInvoke(entry.Delay, entry.Callback));
+                continue;
+            }
+            entry.Callback?.Invoke();
+        }
+    }
+
+    private IEnumerator DelayedInvoke(float delay, UnityEvent callback)
+    {
+        yield return new WaitForSeconds(delay);
+        callback?.Invoke();
     }
 }
